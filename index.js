0925153e8c803fcb9862ad2de788d9cd11b08b7a
Luke Gill <bagrad007@gmail.com>
2021-04-26 11:10:33 -0400

    commit tree f12c6a8c6973e599752366ef12ae531c1626c626

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
 // Your code here
+function createEmployeeRecord(arr) {
+    let obj = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return obj
+}
+
+
+function createEmployeeRecords(arr) {
+    let newArr = []
+    arr.forEach(element => {
+        newArr.push(createEmployeeRecord(element))
+    });
+    return newArr
+}
+
+function createTimeInEvent(obj, dateStamp) {
+    let [date, hour] = dateStamp.split(" ")
+
+    obj.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return obj
+}
+
+function createTimeOutEvent(obj, dateStamp) {
+    let [date, hour] = dateStamp.split(" ")
+
+    obj.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return obj
+}
+
+function hoursWorkedOnDate(obj, dateStamp) {
+    let inEvent = obj.timeInEvents.find(function (e) {
+        return e.date === dateStamp
+    })
+
+    let outEvent = obj.timeOutEvents.find(function (e) {
+        return e.date === dateStamp
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+
+}
+
+function wagesEarnedOnDate(obj, dateStamp) {
+    let save = hoursWorkedOnDate(obj, dateStamp)
+    return save * obj.payPerHour
+}
+
+function allWagesFor(obj) {
+    let dates = obj.timeInEvents.map(function (element) {
+        return element.date
+    })
+
+    let pay = dates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate(obj, d)
+    }, 0)
+
+
+    return pay
+}
+
+
+function findEmployeeByFirstName(source, firstName) {
+    return source.find(element => { return element.firstName === firstName })
+}
+
+
+function calculatePayroll(array) {
+    return array.reduce(function (memo, rec) {
+        return memo + allWagesFor(rec)
+    }, 0)
+}
